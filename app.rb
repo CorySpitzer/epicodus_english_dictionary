944d3d942a07f9b409b81fc131f6f4bc205d9d28
= <coryspitzer@gmail.com>
2015-05-01 12:33:32 -0700

    Display input forms and write logic for displaying entries

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require('sinatra')
 require('sinatra/reloader')
+require('./lib/word')
 also_reload('./lib/*.rb')
 
 get('/') do
+  @words = Word.all()
+  erb(:index)
+end
+
+post('/') do
+  word = params.fetch('word')
+  definition = params.fetch('definition')
+  entry = Word.new(word)
+  entry.add_definition(definition)
+  entry.save()
   erb(:index)
 end
