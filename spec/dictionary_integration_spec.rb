03b5ecc1d80048fe2fce14c2bf628ff8294f1e02
= <coryspitzer@gmail.com>
2015-05-01 14:40:14 -0700

    Test Word.clear method explicitly

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -9,4 +9,15 @@ describe('the dictionary app', :type => :feature) do
       expect(page).to(have_content('A very tiny dictionary'))
     end
   end
+
+  describe('the add dictionary entry path') do
+    it('fills in a word and definition and
+        displays a link to the page of that word') do
+      visit('/')
+      fill_in('word', :with => 'cat')
+      fill_in('definition', :with => 'A feline animal')
+      click_button('Add')
+      expect(page).to(have_content('Success'))
+    end
+  end
 end
