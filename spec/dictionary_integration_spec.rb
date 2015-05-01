f436aad803fb0a6f0c78125a6ad60ec9140c301a
= <coryspitzer@gmail.com>
2015-05-01 11:18:56 -0700

    Test root path

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -1,3 +1,12 @@
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
+
+describe('the dictionary app', :type => :feature) do
+  describe('the root path') do
+    it('visits the root path') do
+      visit('/')
+      expect(page).to(have_content('A very tiny dictionary'))
+    end
+  end
+end
